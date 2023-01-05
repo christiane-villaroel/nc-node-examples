56f65fefc61c20e5d093235b8be26f9cf8a98e0d
Christiane Villaroel <christianevillaroel@gmail.com>
2023-01-05 13:02:58 -0500

    Node Modules Examples

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-const rectangle = {
-    perimeter: (x,y)=> 2*(x+y),
-    area:(x,y)=> x*y
-}
+const rectangle = require(`./rectangle`)
 
 function solveRect (l,w){
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
